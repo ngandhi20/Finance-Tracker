2b41df37ebcd685028dd185857710a1e5cb9d0bb
Nili Gandhi <n.gandhi7788@gmail.com>
2025-11-05 19:06:01 -0700

    built extremely basic structure of my program to refresh my memory of python

--- finance_project.py
+++ finance_project.py
@@ -14,11 +14,47 @@ c.execute("""CREATE TABLE transactions (
 
 conn.commit()
 conn.close()
-"hello"
 
-def main():
+
+def exp_category(amt: float) -> str:
+    print("\nSelect the expense category it lies under!")
+    print("Shopping | Food | Social Life | Bills |")
+    exp_cat = input("\nPlease enter the expense category for this amount: ") 
+    result = f"\nthe expense category for ${abs(amt)} is {exp_cat}"
+    
+    return result
+
+
+def inc_category(amt: float) -> str:
+    print("\nSelect the income category it lies under!")
+    print("Salary | Socials")
+    inc_cat = input("\nPlease enter the income category for this amount: ")
+    result = f"\nthe income category for ${abs(amt)} is {inc_cat}"
+    
+    return result
+
+
+def transaction_type(amt):
+    '''orders amount as income or expense'''
+    if amt < 0:
+        return exp_category(amt)
+    elif amt > 0:
+        return inc_category(amt)
+
+
+def main() -> None:
     '''this is the main part of my code'''
-    amt = input("enter the amount: ")
-    category = input(f"Is {amt} your income or expense?: ")
+    amt = '0'
+    amt = str(input("\nenter the amount: "))
+    while amt == '0':
+        print("Please enter your amount!")
+        amt = str(input("enter the amount: "))
+    
+    while amt != '':
+        result = transaction_type(float(amt))
+        print(result)
+        amt = str(input("\nenter the amount: "))
+    
+    print("\nok bye bye!")
 
 main()
