9d4734034b7882be5f2cd65245a2b92e4b77714b
Nili Gandhi <n.gandhi7788@gmail.com>
2025-11-11 11:21:41 -0700

    testing different python functions

--- finance_project.py
+++ finance_project.py
@@ -24,29 +24,55 @@ def get_trans_by_date(date):
 	
 	
 def update_amount(trans, amount):
-	with conn:
-		c.execute("""UPDATE Transactions SET amount = :amount
-								 WHERE date = :date AND category = :category""",
-								 {'amount': amount, 'date': trans.date, 'category': trans.category})
+    with conn:
+        c.execute("""UPDATE transactions SET amount = :amount
+                  WHERE date = :date AND category = :category""",
+                  {'amount': amount, 'date': trans.date, 'category': trans.category})
+    
+    trans.amount = amount
 
-'''
+
 def remove_trans(trans):
 	with conn:
-		c.execute("DELETE from transactions WHERE date = :date AND category last = :last",
-							{'first': emp.first, 'last': emp.last})
-need to figure out how to write this function'''
+		c.execute("DELETE from transactions WHERE date = :date AND category = :category AND amount = :amount",
+							{'date': trans.date,  'category': trans.category, 'amount': trans.amount})
 
 
-trans_1 = Transactions('2025-11-11', 26.19, 'expense')
-trans_2 = Transactions('2025-11-10', 1000, 'income')
+def main() -> None:
+    trans_1 = Transactions('2025-11-11', 26.19, 'expense')
+    trans_2 = Transactions('2025-11-10', 1000, 'income')
+    trans_3 = Transactions('2025-11-10', 67.50, 'expense')
+	
+    insert_trans(trans_1)
+    insert_trans(trans_2)
+    insert_trans(trans_3)
+	
+    print("Transactions on 2025-11-11: ")
+    get_by_date = get_trans_by_date('2025-11-11')
+    print(get_by_date)
+	
+    print("\nTransactions on 2025-10-10: ")
+    get_by_date = get_trans_by_date('2025-11-10')
+    print(get_by_date)
+	
+    
+    print(f"\nB4 trans_2 amt is = {trans_2.amount}")
+    update_amount(trans_2, 998)
+    print(f"After amount update, trans_2 amt is = {trans_2.amount}")
+	
+	
+    print(f"\ntrans_3 amt is = {trans_3.amount}")
+	
+    print("Removing trans_3")
+    remove_trans(trans_3)
+	
+    remaining = get_trans_by_date('2025-11-10')
+    print(f"Remaining transactions on 2025-11-10 {remaining}")
 
-insert_trans(trans_1)
-insert_trans(trans_2)
+    conn.close()	
+	
+main()
 
-transactions = get_trans_by_date('2025-11-10')
-print(transactions)
-
-conn.close()
 
 """
 def exp_category(amt: float) -> str:
